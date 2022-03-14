f94bff7b7808999492e4628f65437f0a912751be
zhenfei <zhenfei.zhang@hotmail.com>
2022-03-14 12:02:55 -0400

    test

---
+++ test.sh
@@ -0,0 +1,22 @@
+export DEGREE=10
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=11
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=12
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=13
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=14
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=15
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=16
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=17
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=18
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=19
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
+export DEGREE=20
+cargo test --profile bench bench_evm_circuit_prover -p circuit-benchmarks --features benches  -- --nocapture
